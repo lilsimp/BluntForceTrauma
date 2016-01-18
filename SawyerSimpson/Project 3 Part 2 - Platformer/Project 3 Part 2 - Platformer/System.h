47751c9c7510d0e8feace03a87f2263165216843
Sawyer Simpson <aragon12125@hotmail.com>
2016-01-17 16:28:26 -0800

    Alpha is implemented, I think, but there is only linker errors now

---
+++ SawyerSimpson/Project 3 Part 2 - Platformer/Project 3 Part 2 - Platformer/System.h
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------
+Project Title         :       CS 230: Project 1
+File Name             :       System.h
+Author                :       Sawyer Simpson
+Creation Date         :       1/7/2016
+Purpose               :       Contains prototypes for the System calls
+History
+-1/7/2016    :       Worked on and finished all prototypes.
+-1/7/2016    :       Added file header.
+© Copyright 1996-2016, DigiPen Institute of Technology (USA). All rights reserved.
+----------------------------------------------------------------------------*/
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+void System_Initialize(void);
+
+void System_Exit(void);
+
+#endif
